c7736bb8e37014483c133a05659023970ad17d52
GurleenArneja <gurlen7@gmail.com>
2023-09-10 23:53:23 +0530

    Create Documents.js

---
+++ public/src/components/Documents.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderLinks from './HeaderLinks';
+
+const Documents = () => {
+  return (
+    <>
+      <HeaderLinks header="Documents"/>
+    </>
+  );
+};
+
+export default Documents;
