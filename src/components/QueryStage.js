a59374299d04281fb96d82b07d3ea8094ca77034
GurleenArneja <gurlen7@gmail.com>
2023-09-15 14:20:39 +0530

    Create QueryStage.js

---
+++ src/components/QueryStage.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+
+const QueryStage = ({ steps, setStageSelected }) => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setStageSelected(steps[activeStep + 1]);
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleReset = () => {
+    setStageSelected(steps[0]);
+    setActiveStep(0);
+  };
+
+  return (
+    <Box style={{paddingTop: '20px'}}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+}
+
+export default QueryStage;
