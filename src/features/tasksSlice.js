a3995e7cc7d1211446aea6d0993b77e753f896f9
GurleenArneja <gurlen7@gmail.com>
2023-09-15 14:21:36 +0530

    Update tasksSlice.js

--- src/features/tasksSlice.js
+++ src/features/tasksSlice.js
@@ -1,8 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const tasks = [
-    "Suppliers Who Kept Orders Waiting Query (Q21)"
-]
+const tasks = {
+    "Suppliers Who Kept Orders Waiting Query (Q21)": {
+        stages: 5,
+        stageSchema: {
+            stage1: "Orders",
+            stage2: "Lineitem",
+            stage3: "Lineitem",
+            stage4: "Q21",
+            stage5: "Q21"
+        }
+    }
+} 
 
 const tasksSlice = createSlice({
     name: 'tasks',
