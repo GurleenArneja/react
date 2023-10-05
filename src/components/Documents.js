e7aebf031836a0684dcddf1fd484be8e91fed70b
GurleenArneja <gurlen7@gmail.com>
2023-10-05 22:27:44 +0530

    Update Documents.js

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import HeaderLinks from './HeaderLinks';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar
+} from "@mui/material";
+
+import { useSelector } from "react-redux";
 
 const Documents = () => {
+  const { tables } = useSelector((state) => state.tpch);
   return (
     <>
-      <HeaderLinks header="Documents"/>
+      <HeaderLinks header="Documents" />
+      <div className='mainContent'>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          {tables.map((table, index) => {
+            return <ListItem key={index}>
+              <ListItemAvatar>
+                <Avatar>
+                  20
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={table}/>
+            </ListItem>
+          })}
+        </List>
+      </div>
     </>
   );
 };
