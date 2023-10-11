923b16a655aebb030f2b5a429b6ba36447c251b7
GurleenArneja <gurlen7@gmail.com>
2023-10-11 14:38:24 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import SignIn from './components/SignIn';
 import Dashboard from './components/Dashboard';
 import SharedLayout from './components/SharedLayout';
 import Collections from './components/Collections';
 import Documents from './components/Documents';
 import Tasks from './components/Tasks';
-import User from './components/User';
+import { useSelector } from 'react-redux';
 
 const AppRoutes = () => {
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<SharedLayout/>}>
-            <Route index element={<Dashboard/>}/>
-            <Route path='collections' element={<Collections/>}/>
-            <Route path='tasks' element={<Tasks/>}/>
-            <Route path='documents' element={<Documents/>}/>
-            <Route path='user' element={<User/>}/>
-          </Route>
-        </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <Navigate to="/tpch" /> : <SignIn/>}
+        />
+        <Route
+          path="/tpch"
+          element={isLoggedIn ? <SharedLayout /> : <Navigate to="/" />}
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="collections" element={<Collections />} />
+          <Route path="tasks" element={<Tasks />} />
+          <Route path="documents" element={<Documents />} />
+        </Route>
+        <Route path="/logout" element={<SignIn />} />
+      </Routes>
     </BrowserRouter>
   );
 };
