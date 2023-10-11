0652bc26c9f43ea09e0b44c4bba3929358dfcb04
GurleenArneja <gurlen7@gmail.com>
2023-10-11 14:46:52 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import tpchReducer from './features/tpchSlice'
 import ordersReducer from "./features/ordersSlice";
 import tasksReducer from './features/tasksSlice';
+import authReducer from "./features/authSlice";
 
 export const store = configureStore({
     reducer: {
         tpch: tpchReducer,
         orders: ordersReducer,
-        tasks: tasksReducer
+        tasks: tasksReducer,
+        auth: authReducer
     }
 });
