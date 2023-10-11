122da9f49dc73b0cd642a644cedf18b68ff73a70
GurleenArneja <gurlen7@gmail.com>
2023-10-11 14:40:12 +0530

    Update Documents.js

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -9,24 +9,29 @@ import {
 } from "@mui/material";
 
 import { useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 
 const Documents = () => {
   const { tables } = useSelector((state) => state.tpch);
+  const navigate = useNavigate();
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  if (!isLoggedIn) {
+    navigate('/');
+  }
   return (
     <>
       <HeaderLinks header="Documents" />
       <div className='mainContent'>
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          {tables.map((table, index) => {
-            return <ListItem key={index}>
+          <ListItem>
               <ListItemAvatar>
-                <Avatar>
-                  20
+                <Avatar style={{width: "60px", height: '60px'}}>
+                  1500
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText primary={table}/>
+              <ListItemText primary="Orders" style={{paddingLeft: "10px"}}/>
             </ListItem>
-          })}
         </List>
       </div>
     </>
