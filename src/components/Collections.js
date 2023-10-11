e25d73084705f2c5e3e24813d0e760717d090102
GurleenArneja <gurlen7@gmail.com>
2023-10-11 14:39:11 +0530

    Update Collections.js

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import HeaderLinks from './HeaderLinks';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Collections = () => {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  if (!isLoggedIn) {
+    navigate('/');
+  }
   return (
     <>
       <HeaderLinks header="Collections"/>
