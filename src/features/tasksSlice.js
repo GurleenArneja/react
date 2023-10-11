b2cb4f9a5eae1d29885c4fc0f060e7ac67d2cb9b
GurleenArneja <gurlen7@gmail.com>
2023-10-11 14:45:06 +0530

    Update tasksSlice.js

--- src/features/tasksSlice.js
+++ src/features/tasksSlice.js
@@ -1,16 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const tasks = {
+    "Large Volume Customer Query (Q18)": {},
+    "Discounted Revenue Query (Q19)": {},
+    "Potential Part Promotion Query (Q20)": {},
     "Suppliers Who Kept Orders Waiting Query (Q21)": {
         stages: 5,
         stageSchema: {
             stage1: "Orders",
-            stage2: "Lineitems",
-            stage3: "Lineitems",
+            stage2: "Lineitem",
+            stage3: "Lineitem",
             stage4: "Q21",
             stage5: "Q21"
         }
-    }
+    },
+    "Global Sales Opportunity Query (Q22)": {}
 } 
 
 const tasksSlice = createSlice({
